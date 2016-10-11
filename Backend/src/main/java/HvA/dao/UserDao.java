511b583027fc33470347ce241171b41614efda79
Kadir Basturk <kbasturk@hotmail.com>
2016-10-11 21:34:57 +0200

    modified:   Backend/build.gradle 	modified:   Backend/src/main/java/HvA/Database/DatabaseConfiguration.java 	modified:   Backend/src/main/java/HvA/GreetingController.java 	new file:   Backend/src/main/java/HvA/dao/UserDao.java 	modified:   Backend/src/main/java/HvA/model/User.java

---
+++ Backend/src/main/java/HvA/dao/UserDao.java
@@ -0,0 +1,31 @@
+package HvA.dao;
+
+import HvA.model.User;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ * Project: Backend,
+ * Created by Kadir Basturk on 11-10-2016.
+ */
+@Repository
+public class UserDao {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Transactional
+    public User getUser(int id) {
+        return em.createNamedQuery("User.find", User.class).setParameter("id", id).getSingleResult();
+    }
+
+//    @Transactional
+//    public void saveUser(User user) {
+//        em.persist(user);
+//        em.flush();
+//    }
+
+}
