ad96b994fc86d0b690a6daa26eced718a9a4b4a2
Roemer Bakker <contact@roemerbakker.com>
2016-10-25 12:25:46 +0200

    Created getCourse API method

--- Backend/src/main/java/HvA/Controllers/CourseController.java
+++ Backend/src/main/java/HvA/Controllers/CourseController.java
@@ -2,10 +2,7 @@ package HvA.Controllers;
 
 import HvA.model.Course;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -22,8 +19,8 @@ public class CourseController
     @Autowired
     private CourseDao dao;
 
-    @RequestMapping("/course/{id}")
-    public Course getCourse(@RequestParam(value = "id", required = true) @PathVariable("id") int id)
+    @RequestMapping(value = "/course/{id}", method = RequestMethod.GET)
+    public Course getCourse(@PathVariable("id") int id)
     {
         return dao.getCourse(id);
     }
--- Backend/src/main/java/HvA/model/Course.java
+++ Backend/src/main/java/HvA/model/Course.java
@@ -28,4 +28,79 @@ public class Course
 
     @Column(name = "type")
     private String type;
+
+
+    public Course(int id, int studyProgramId, int semester, String name, int ECTS, String type)
+    {
+        this.id = id;
+        this.studyProgramId = studyProgramId;
+        this.semester = semester;
+        this.name = name;
+        this.ECTS = ECTS;
+        this.type = type;
+    }
+
+    public Course()
+    {
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public int getStudyProgramId()
+    {
+        return studyProgramId;
+    }
+
+    public void setStudyProgramId(int studyProgramId)
+    {
+        this.studyProgramId = studyProgramId;
+    }
+
+    public int getSemester()
+    {
+        return semester;
+    }
+
+    public void setSemester(int semester)
+    {
+        this.semester = semester;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public int getECTS()
+    {
+        return ECTS;
+    }
+
+    public void setECTS(int ECTS)
+    {
+        this.ECTS = ECTS;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
 }
