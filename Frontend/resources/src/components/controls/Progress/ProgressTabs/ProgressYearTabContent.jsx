c26a60b0c9a9df52e8e10348698883ded561cbf9
Roemer Bakker <contact@roemerbakker.com>
2017-01-14 21:17:34 +0100

    Added comments and removed console.log

--- Frontend/resources/src/components/controls/Progress/ProgressTabs/ProgressYearTabContent.jsx
+++ Frontend/resources/src/components/controls/Progress/ProgressTabs/ProgressYearTabContent.jsx
@@ -6,40 +6,53 @@ import Tab from 'react-bootstrap/lib/Tab';
 export default class ProgressYearTabContent extends React.Component {
     constructor(props) {
         super(props);
-
-        console.log('props', this.props);
     }
 
+    /**
+     * Returns the first semester for the defaultActiveKey of the tabs
+     *
+     * @returns {*}
+     */
     getFirstSemester() {
-        for(var semester in this.props.semesters) {
-            return this.props.semesters[semester];
+        for(var semester in this.props.semesters) { // For each semester prop
+            return this.props.semesters[semester]; // Return the first one right away
         }
     }
 
+    /**
+     * Renders the tabs. Only creates tab if a semester has courses
+     *
+     * @returns {Array}
+     */
     renderSemesterTabs() {
-        var tabs = [];
-        var coursesPerSemester = [];
+        var tabs = []; // The tabs to be returned
+        var coursesPerSemester = []; // Courses will be devided between their semesters
 
-        for(var course in this.props.courses) {
-            var courseObject = this.props.courses[course];
-            if (coursesPerSemester[courseObject.semester] == undefined)
-                coursesPerSemester[courseObject.semester] = [];
+        for(var course in this.props.courses) { // For each course
+            var courseObject = this.props.courses[course]; // Save course object for readability
+            if (coursesPerSemester[courseObject.semester] == undefined) // Check if the semester exists already
+                coursesPerSemester[courseObject.semester] = []; // If not, create an empty array for courses
 
-            coursesPerSemester[courseObject.semester][courseObject.id] = courseObject;
+            coursesPerSemester[courseObject.semester][courseObject.id] = courseObject; // Save course to the array
         }
 
-        for(var semester in coursesPerSemester) {
-            var tab =
+        for(var semester in coursesPerSemester) { // For reach semester
+            var tab = // Define the tab
                 <Tab eventKey={'semester_' + semester} key={'semester_' + semester} title={'Semester ' + semester}>
                     Content!
                 </Tab>
 
-            tabs.push(tab);
+            tabs.push(tab); // Push the tab onto the array
         }
 
         return tabs;
     }
 
+    /**
+     * Render the component
+     *
+     * @returns {HTML}
+     */
     render() {
         return (
             <Tabs defaultActiveKey={'semester_' + this.getFirstSemester()} id="progressSemesterTabs">
