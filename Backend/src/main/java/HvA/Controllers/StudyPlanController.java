16942cf797aa7b1f5897f7c76d36736b8b197219
Roemer Bakker <contact@roemerbakker.com>
2017-01-25 21:31:22 +0100

    Reverted study plan controller

--- Backend/src/main/java/HvA/Controllers/StudyPlanController.java
+++ Backend/src/main/java/HvA/Controllers/StudyPlanController.java
@@ -1,7 +1,9 @@
 package HvA.Controllers;
 
 import HvA.dao.StudyPlanDao;
+import HvA.dao.UserDao;
 import HvA.model.Study_Plan;
+import HvA.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -23,6 +25,9 @@ public class StudyPlanController
      */
     @Autowired
     private StudyPlanDao dao;
+
+    @Autowired
+    private UserDao userDao;
 
     /**
      * Get a specific study program
@@ -63,12 +68,31 @@ public class StudyPlanController
      * @param Study_Plan studyplan
      * @return ResponseEntity<Study_Plan>
      */
-    @RequestMapping(value = "/studyplan/create", method = RequestMethod.POST)
-    public ResponseEntity<Study_Plan> createStudyPlan(@RequestBody Study_Plan studyplan)
+    @RequestMapping(value = "/studyplan", method = RequestMethod.POST)
+    public ResponseEntity<Study_Plan> createStudyPlan(
+            @RequestBody Study_Plan studyplan,
+            @RequestParam("user_id") int userId)
     {
-        dao.createStudyPlan(studyplan);
+        User user = null;
+        try
+        {
+            user = userDao.getUser(userId);
+        }
+        catch(NoResultException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
 
-        return new ResponseEntity<Study_Plan>(studyplan, HttpStatus.OK);
+        Study_Plan newPlan = new Study_Plan();
+        newPlan.setAccepted(true);
+        newPlan.setSemester4(studyplan.getSemester4());
+        newPlan.setSemester5(studyplan.getSemester5());
+        newPlan.setMinor(studyplan.getMinor());
+        newPlan.setUser(user);
+
+        dao.createStudyPlan(newPlan);
+
+        return new ResponseEntity<Study_Plan>(newPlan, HttpStatus.OK);
     }
 
     @RequestMapping(value ="/studyplan/{id}/delete", method = RequestMethod.DELETE)
